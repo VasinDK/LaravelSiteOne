1b4e64e02257a634ef3cabe30009d67b929c1363
Vasin_D <vasindima@ya.ru>
2020-06-13 14:58:21 +0300

    New branch start

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
 <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
 <link href="{{asset('assets/css/style.css')}}" rel="stylesheet" type="text/css"> 
 
-<script type="text/javascript" src="{{asset('assets/js/jquery-1.11.0.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('assets/json_decode(json)/jquery-1.11.0.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/js/ckeditor/ckeditor.js')}}"></script>  {{-- Подключение текстового редактора  да --}}
 <script type="text/javascript" src="{{asset('assets/js/bootstrap.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/js/bootstrap-filestyle.min.js')}}"></script>

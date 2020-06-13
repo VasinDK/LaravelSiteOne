c5f0969fd4d0de00bf958152327221914bdbd53c
Vasin_D <vasindima@ya.ru>
2020-06-13 17:15:01 +0300

    Content_pages

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,8 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+/*use App\Page;*/
 
 class PagesController extends Controller
 {
     //
+    public function execute () {
+
+    	if(view()->exists('admin.pages')){
+    		$pages = \App\Page::all();  // вмсто: use App\Page , в самсм начале
+
+    		$data = [
+    			'title' => 'Страницы',
+    			'pages' => $pages
+    		];
+
+    		return view('admin.pages', $data);
+    	} abort(404);
+
+    }
 }

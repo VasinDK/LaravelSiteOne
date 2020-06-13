6e9afdcd93e3b2bbd84bfb1bd44b4358c29c9ce0
Vasin_D <vasindima@ya.ru>
2020-06-13 13:08:10 +0300

    next exgenge

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,7 +9,7 @@
 <link href="{{asset('assets/css/style.css')}}" rel="stylesheet" type="text/css"> 
 
 <script type="text/javascript" src="{{asset('assets/js/jquery-1.11.0.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/ckeditor/ckeditor.js')}}"></script>  {{-- Подключение текстового редактора   --}}
+<script type="text/javascript" src="{{asset('assets/js/ckeditor/ckeditor.js')}}"></script>  {{-- Подключение текстового редактора  да --}}
 <script type="text/javascript" src="{{asset('assets/js/bootstrap.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/js/bootstrap-filestyle.min.js')}}"></script>
  

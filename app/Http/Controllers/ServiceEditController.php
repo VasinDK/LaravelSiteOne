261929d313d8662cc33d41feca3affb9b9330f03
Vasin_D <vasindima@ya.ru>
2020-06-26 15:54:31 +0300

    add second exemple service

--- app/Http/Controllers/ServiceEditController.php
+++ app/Http/Controllers/ServiceEditController.php
@@ -6,18 +6,40 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Service;
+use App\Validator;
 
 class ServiceEditController extends Controller
 {
     //Изменение услуги
     public function execute(Service $service, Request $request){
+
+        // Записываем данные в модель
+        if($request-> isMethod('post')){
+
+            $input = $request-> except('_token');    
+            $message = [
+                'required' => 'Поле :attrebute обязательно к заполнению',
+                'max' => 'Поле :attrebute должно иметь не более 255 символов'
+            ];        
+
+            $validator = Validator::make($input, [
+                    'name' => 'required|max:255', 
+                    'text' =>  'required'
+                ], $message;
+
+            if($validator-> fails){
+                return redirect('')
+            }
+            $service = 0;
+        }
+
         // Получаем данные из БД
         $old = $service-> toArray();
         $data = [
             'title' => 'Редактирование услуги ' . $old['name'],
-            'old' => $old
+            'data' => $old
         ];
-        if(view()->exist('admin.services_edit')){
+        if(view()->exists('admin.services_edit')){
             return view('admin.services_edit', $data);
         }
 
--- resources/views/admin/content_services_edit.blade.php
+++ resources/views/admin/content_services_edit.blade.php
@@ -1,6 +1,6 @@
 {{-- форма редактирования портфолио --}}
 <div class="wrapper container-fluid">
-    {!! Form::open(['url' = route('servicesEdit', ['service' => $data['id']]),'class'=>'form-horizontal', 'method' = 'POST', ,'enctype'=>'multipart/form-data']) !!}
+    {!! Form::open(['url' => route('servicesEdit', array('service'=>$data['id'])),'class' => 'form-horizontal', 'method' => 'POST','enctype'=>'multipart/form-data']) !!}
 
     <div class="form-group">
         {!! Form::hidden('id', $data['id']) !!}
@@ -18,20 +18,31 @@
     </div>
 
     <div class="form-group">
-        {!! Form::label('old_images', 'Изображение:',['class'=>'col-xs-2 control-label']) !!}
+        {!! Form::label('old_icon', 'Изображение:',['class'=>'col-xs-2 control-label']) !!}
         <div class="col-xs-offset-2 col-xs-10">
-            {!! Html::image('assets/img/'.$data['images'],'',['class'=>'img-circle img-responsive','width'=>'150px']) !!}
-            {!! Form::hidden('old_images', $data['images']) !!}
+            {!! Html::image('assets/img/'. $data['icon'],'',['class'=>'img-circle img-responsive','width'=>'150px']) !!}
+            {!! Form::hidden('old_icon', $data['icon']) !!}
         </div>
     </div>
 
     <div class="form-group">
-         {!! Form::label('images', 'Изображение:',['class'=>'col-xs-2 control-label']) !!}
+         {!! Form::label('icon', 'Изображение:',['class'=>'col-xs-2 control-label']) !!}
          <div class="col-xs-8">
-            {!! Form::file('images', ['class' => 'filestyle','data-buttonText'=>'Выберите изображение','data-buttonName'=>"btn-primary",'data-placeholder'=>"Файла нет"]) !!}
+            {!! Form::file('icon', ['class' => 'filestyle','data-buttonText'=>'Выберите изображение','data-buttonName'=>"btn-primary",'data-placeholder'=>"Файла нет"]) !!}
          </div>
+    </div>
+    
+    <div class="form-group">
+        <div class="col-xs-offset-2 col-xs-10">
+          {!! Form::button('Сохранить', ['class' => 'btn btn-primary','type'=>'submit']) !!}
+        </div>
     </div>
 
 
     {!! Form::close() !!}
+
+ <script>
+    CKEDITOR.replace( 'editor' );   
+</script>
+    
 </div>
